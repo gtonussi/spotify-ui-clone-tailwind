6632bcd4d4662d49d288122c7eeaa8519a7737b4
Giovanni Tonussi <gtonussi@uolinc.com>
2023-05-17 19:47:47 -0300

    monocommit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,332 @@
-import Image from 'next/image'
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  HomeIcon,
+  Laptop2Icon,
+  LayoutListIcon,
+  LibraryIcon,
+  Maximize2Icon,
+  Mic2Icon,
+  PlayIcon,
+  RepeatIcon,
+  SearchIcon,
+  ShuffleIcon,
+  SkipBackIcon,
+  SkipForwardIcon,
+  VolumeIcon,
+} from "lucide-react";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <aside className="w-72 bg-zinc-950 p-6">
+          <div className="flex items-center gap-2">
+            <div className="w-3 h-3 bg-red-500 rounded-full"></div>
+            <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
+            <div className="w-3 h-3 bg-green-500 rounded-full"></div>
+          </div>
+
+          <nav className="space-y-5 mt-10">
+            <a
+              href=""
+              className="flex items-center text-sm font-semibold text-zinc-200 gap-3"
+            >
+              <HomeIcon />
+              Home
+            </a>
+            <a
+              href=""
+              className="flex items-center text-sm font-semibold text-zinc-200 gap-3"
+            >
+              <SearchIcon />
+              Search
+            </a>
+            <a
+              href=""
+              className="flex items-center text-sm font-semibold text-zinc-200 gap-3"
+            >
+              <LibraryIcon />
+              Your Library
+            </a>
+          </nav>
+
+          <nav className="mt-6 pt-10 border-t border-zinc-800 flex flex-col gap-3">
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Minimal Study Techno
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              House Music 2023
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Bullet Holes Radio
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              420Moments
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Pop Up
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Code Mode
+            </a>
+          </nav>
+        </aside>
+
+        <main className="flex-1 p-6">
+          <div className="flex items-center gap-4">
+            <button className="rounded-full bg-black/40 p-2">
+              <ChevronLeftIcon />
+            </button>
+            <button className="rounded-full bg-black/40 p-2">
+              <ChevronRightIcon />
+            </button>
+          </div>
+
+          <h1 className="font-semibold text-3xl mt-10">Good Afternoon</h1>
+
+          <div className="grid grid-cols-3 gap-4 mt-4">
+            <a
+              href=""
+              className="group group bg-white/5 rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                alt="Capa do álbum Wish You Were Here"
+                height={104}
+                src="/images/wywh.jpg"
+                width={104}
+              />
+              <strong>Wish You Were Here</strong>
+
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <PlayIcon />
+              </button>
+            </a>
+            <a
+              href=""
+              className="group bg-white/5 rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                alt="Capa do álbum Wish You Were Here"
+                height={104}
+                src="/images/wywh.jpg"
+                width={104}
+              />
+              <strong>Wish You Were Here</strong>
+
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <PlayIcon />
+              </button>
+            </a>
+            <a
+              href=""
+              className="group bg-white/5 rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                alt="Capa do álbum Wish You Were Here"
+                height={104}
+                src="/images/wywh.jpg"
+                width={104}
+              />
+              <strong>Wish You Were Here</strong>
+
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <PlayIcon />
+              </button>
+            </a>
+            <a
+              href=""
+              className="group bg-white/5 rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                alt="Capa do álbum Wish You Were Here"
+                height={104}
+                src="/images/wywh.jpg"
+                width={104}
+              />
+              <strong>Wish You Were Here</strong>
+
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <PlayIcon />
+              </button>
+            </a>
+            <a
+              href=""
+              className="group bg-white/5 rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/images/wywh.jpg"
+                alt="Capa do álbum Wish You Were Here"
+                width={104}
+                height={104}
+              />
+              <strong>Wish You Were Here</strong>
+
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <PlayIcon />
+              </button>
+            </a>
+            <a
+              href=""
+              className="group bg-white/5 rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/images/wywh.jpg"
+                alt="Capa do álbum Wish You Were Here"
+                width={104}
+                height={104}
+              />
+              <strong>Wish You Were Here</strong>
+
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <PlayIcon />
+              </button>
+            </a>
+          </div>
+
+          <h2 className="font-semibold text-2xl mt-10">Made for gtonussi</h2>
+
+          <div className="grid grid-cols-8 gap-4 mt-4">
+            <a
+              href=""
+              className="bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-2"
+            >
+              <Image
+                alt="Capa do álbum Wish You Were Here"
+                className="w-full"
+                height={120}
+                src="/images/wywh.jpg"
+                width={120}
+              />
+
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-500">
+                Awake at Last, 10 Years, Bush and more
+              </span>
+            </a>
+            <a
+              href=""
+              className="bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-2"
+            >
+              <Image
+                alt="Capa do álbum Wish You Were Here"
+                className="w-full"
+                height={120}
+                src="/images/wywh.jpg"
+                width={120}
+              />
+
+              <strong className="font-semibold">Daily Mix 2</strong>
+              <span className="text-sm text-zinc-500">
+                Lone Tusker, Tiësto, Enzo Faleiro and more
+              </span>
+            </a>
+            <a
+              href=""
+              className="bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-2"
+            >
+              <Image
+                alt="Capa do álbum Wish You Were Here"
+                className="w-full"
+                height={120}
+                src="/images/wywh.jpg"
+                width={120}
+              />
+
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-500">
+                Awake at Last, 10 Years, Bush and more
+              </span>
+            </a>
+            <a
+              href=""
+              className="bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-2"
+            >
+              <Image
+                alt="Capa do álbum Wish You Were Here"
+                className="w-full"
+                height={120}
+                src="/images/wywh.jpg"
+                width={120}
+              />
+
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-500">
+                Awake at Last, 10 Years, Bush and more
+              </span>
+            </a>
+            <a
+              href=""
+              className="bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-2"
+            >
+              <Image
+                alt="Capa do álbum Wish You Were Here"
+                className="w-full"
+                height={120}
+                src="/images/wywh.jpg"
+                width={120}
+              />
+
+              <strong className="font-semibold">Daily Mix 2</strong>
+              <span className="text-sm text-zinc-500">
+                Lone Tusker, Tiësto, Enzo Faleiro and more
+              </span>
+            </a>
+          </div>
+        </main>
+      </div>
+
+      <footer className="bg-zinc-800 border-t border-zinc-700 px-6 py-4 flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <Image
+            alt="Capa do álbum Wish You Were Here"
+            height={56}
+            src="/images/wywh.jpg"
+            width={56}
+          />
+
+          <div className="flex flex-col">
+            <strong className="font-normal">Welcome to the Machine</strong>
+            <span className="text-xs text-zinc-400">Pink Floyd</span>
+          </div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className="flex flex-col items-center gap-2">
+          <div className="flex items-center gap-6">
+            <ShuffleIcon size={20} className="text-zinc-200" />
+            <SkipBackIcon size={20} className="text-zinc-200" />
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black">
+              <PlayIcon size={20} />
+            </button>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+            <SkipForwardIcon size={20} className="text-zinc-200" />
+            <RepeatIcon size={20} className="text-zinc-200" />
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+          <div className="flex items-center gap-2">
+            <span className="text-xs text-zinc-500">0:31</span>
+            <div className="h-1 rounded-full w-96 bg-zinc-600">
+              <div className="bg-zinc-200 w-40 h-1 rounded-full"></div>
+            </div>
+            <span className="text-xs text-zinc-500">7:25</span>
+          </div>
+        </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <div className="flex items-center gap-4">
+          <Mic2Icon size={20} />
+          <LayoutListIcon size={20} />
+          <Laptop2Icon size={20} />
+          <div className="flex items-center gap-4">
+            <VolumeIcon size={20} />
+            <div className="h-1 rounded-full w-24 bg-zinc-600">
+              <div className="bg-zinc-200 w-10 h-1 rounded-full"></div>
+            </div>
+          </div>
+          <Maximize2Icon size={20} />
+        </div>
+      </footer>
+    </div>
+  );
 }
